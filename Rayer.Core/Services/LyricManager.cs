f18007735aaf0c24fc67c495193e83e7b3e08d1b
MM丶 <820245770@qq.com>
2025-05-14 15:06:57 +0800

    chore：优化歌词偏移存储区分不同的歌词搜索器

--- Rayer.Core/Services/LyricManager.cs
+++ Rayer.Core/Services/LyricManager.cs
@@ -1,5 +1,7 @@
 ﻿using Rayer.Core.Abstractions;
+using Rayer.Core.Common;
 using Rayer.Core.Framework.Injection;
+using Rayer.Core.Framework.Settings.Abstractions;
 using Rayer.Core.Models;
 using System.Collections.Concurrent;
 using System.IO;
@@ -9,49 +11,53 @@ namespace Rayer.Core.Services;
 [Inject<ILyricManager>]
 internal sealed class LyricManager : ILyricManager
 {
-    private readonly ConcurrentDictionary<string, int> _offsetMap = [];
+    private readonly ISettingsService _settingsService;
+    private readonly ConcurrentDictionary<LyricKey, int> _offsetMap = [];
 
-    public LyricManager()
+    public LyricManager(ISettingsService settingsService)
     {
+        _settingsService = settingsService;
+
         if (!Directory.Exists(Constants.Paths.LyricPath))
         {
             Directory.CreateDirectory(Constants.Paths.LyricPath);
         }
         else
         {
-            _offsetMap = new ConcurrentDictionary<string, int>(Directory.EnumerateFiles(Constants.Paths.LyricPath, "*")
+            _offsetMap = new ConcurrentDictionary<LyricKey, int>(Directory.EnumerateFiles(Constants.Paths.LyricPath, "*")
                 .Select(x => x.Split('_'))
-                .ToDictionary(k => Path.GetFileNameWithoutExtension(k[0]), v => int.Parse(v[1])));
+                .ToDictionary(k => new LyricKey(Path.GetFileNameWithoutExtension(k[0]), (LyricSearcher)int.Parse(k[1])), v => int.Parse(v[2])));
         }
     }
 
     public void Store(Audio audio, int offset)
     {
         var md5 = GetMD5(audio);
+        var filenamePrefix = $"{md5}_{(int)_settingsService.Settings.LyricSearcher}";
 
         int finalOffset = offset;
-        var files = Directory.GetFiles(Constants.Paths.LyricPath, $"{md5}*", SearchOption.TopDirectoryOnly);
+        var files = Directory.GetFiles(Constants.Paths.LyricPath, $"{filenamePrefix}*", SearchOption.TopDirectoryOnly);
         if (files.Length == 0)
         {
-            using var newFile = new FileStream($"{Constants.Paths.LyricPath}/{md5}_{finalOffset}", FileMode.CreateNew, FileAccess.Write, FileShare.None);
+            using var newFile = new FileStream($"{Constants.Paths.LyricPath}/{md5}_{(int)_settingsService.Settings.LyricSearcher}_{finalOffset}", FileMode.CreateNew, FileAccess.Write, FileShare.None);
             newFile.Close();
         }
         else
         {
             var fileInfo = new FileInfo(files[0]);
-            var originalOffset = int.Parse(fileInfo.Name.Split('_')[1]);
+            var originalOffset = int.Parse(fileInfo.Name.Split('_')[2]);
             finalOffset = originalOffset + offset;
-            fileInfo.MoveTo($"{Constants.Paths.LyricPath}/{md5}_{finalOffset}");
+            fileInfo.MoveTo($"{Constants.Paths.LyricPath}/{md5}_{(int)_settingsService.Settings.LyricSearcher}_{finalOffset}");
         }
 
-        _offsetMap.AddOrUpdate(md5, finalOffset, (_, _) => finalOffset);
+        _offsetMap.AddOrUpdate(new LyricKey(md5, _settingsService.Settings.LyricSearcher), finalOffset, (_, _) => finalOffset);
     }
 
     public int LoadOffset(Audio audio)
     {
         var md5 = GetMD5(audio);
 
-        if (_offsetMap.TryGetValue(md5, out var offset))
+        if (_offsetMap.TryGetValue(new LyricKey(md5, _settingsService.Settings.LyricSearcher), out var offset))
         {
             return offset;
         }
@@ -67,4 +73,6 @@ internal sealed class LyricManager : ILyricManager
 
         return md5String.ToLower();
     }
+
+    private record LyricKey(string MD5, LyricSearcher LyricSearcher);
 }
